90f6b6b06b8065a27ef7aeff82f279ca2c01c5fa
Kirill <g4s8.public@gmail.com>
2023-11-07 12:02:35 +0400

    clicky

---
+++ static/cc.js
@@ -0,0 +1,3 @@
+<a title="clicky" href="https://clicky.com/101432902"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script async data-id="101432902" src="//static.getclicky.com/js"></script>
+
