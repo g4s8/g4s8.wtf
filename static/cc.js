1b7725092a7555c6fcb99e6005876db93795234c
Kirill <g4s8.public@gmail.com>
2023-11-07 14:59:57 +0400

    site footer config

--- static/cc.js
+++
@@ -1,3 +0,0 @@
-<a title="clicky" href="https://clicky.com/101432902"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
-<script async data-id="101432902" src="//static.getclicky.com/js"></script>
-
